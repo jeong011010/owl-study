b0743331e1053862452b281f67e389d3a7dca3ef
Kim._.jazz <jeong011010@naver.com>
2022-05-14 16:42:54 +0900

    study NodeJS - router

---
+++ NodeJs/js/express/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express(); // express 사용 형식
+
+app.get('/', function(req, res){ //get 방식으로 접속한 유저가 '/'에 접속했다면 콜백함수 실행
+    res.send('Hello home page'); // res(response)의 send method 인자로 출력하고 싶은 메세지
+});
+app.get('/login',function(req,res){
+    res.send('Login please');
+})
+
+app.listen(3000, function(){ //port 번호와, 연결 성공여부에 따라 실행되는 콜백함수
+    console.log('Connected 3000 port');
+});
