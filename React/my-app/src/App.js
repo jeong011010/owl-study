1b80fd1c9990cdc275d8da32711791dd0152dd34
Kim._.jazz <jeong011010@naver.com>
2022-05-20 08:47:21 +0900

    study React - loop (map)

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Counter from './components/Counter'
 
 function App() {
-  const [condition, setCondition] = useState(false);
-  const toggle = ()  => {
-    setCondition(!condition);
-  }
-  useEffect(() => {
-    console.log(condition);
-  }, [condition])
-  const renderCondition = condition ? 'True':'False';
+  const movies = [
+    {title:'kossie coder1', year:2001},
+    {title:'kossie coder2', year:2002},
+    {title:'kossie coder3', year:2003},
+  ];
+
+  const renderMovies = movies.map(movie => {
+    return (
+      <div className="movie" key={movie.title}>
+          <div className="movie-title">{movie.title}</div>
+          <div className="movie-year">{movie.year}</div>
+        </div>
+    )
+  })
 
   return (
     <div className="App">
-      <h1>Kim Jazz</h1>
-      {renderCondition}<br/>
-      <button onClick={toggle}>Toggle</button>
+      <h1>Movie list</h1>
+        {renderMovies}
     </div>
   );
 }
