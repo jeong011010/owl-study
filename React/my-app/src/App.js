31c896ea0eff051bd9cfd2a0f9f1375dffc6cbff
Kim._.jazz <jeong011010@naver.com>
2022-05-20 03:43:28 +0900

    study React - useEffect

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState(''); 
-
-  const onSubmit = () => {
-    alert('submitted');
-  };
-
+  
+  const [count, setCount] = useState(0);
+  useEffect(()=> {
+    console.log(count)
+  }, [count])
+  console.log('rendering');
 
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-        <input placeholder="Username"
-        value={username}
-        onChange={(e)=> setUsername(e.target.value)}
-        /> <br/>
-        <input placeholder="Password"
-        value={password}
-        onChange={(e)=> setPassword(e.target.value)}
-        /> <br/>
-        <button type="submit">Login</button>
-      </form>
+      <h1>{count}</h1>
+      <button onClick={() => {setCount(count + 1)}}>Click</button>
     </div>
   );
 }
