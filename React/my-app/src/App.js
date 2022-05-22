c7d605993c86114e9a8932cf212f2437e56df498
Kim._.jazz <jeong011010@naver.com>
2022-05-23 02:19:39 +0900

    study React - React Router

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import MovieForm from './components/MovieForm';
 import Navbar from './components/Navbar';
 import {
+  Link,
   BrowserRouter,
   Routes,
   Route,
@@ -12,8 +13,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route path="/movies" element={<MovieForm/>}/>
+          <Route path="/" element={<h1>HOME</h1>}/>
+          <Route path="/users" element={<h1>USERS</h1>}/>
+        
         </Routes>
       </BrowserRouter>
     </div>
