6e45725af62325fc4b62554c79f1e6aa25806af6
Kim._.jazz <jeong011010@naver.com>
2022-05-27 17:32:04 +0900

    study NodeJS - make token, login with bcrypt

--- NodeJs/js2/index.js
+++ NodeJs/js2/index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const {User} = require('./models/User');
 cosnt config = require('./config/key')
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI)
   .then(()=>console.log('MongoDB Connected...'))
@@ -28,6 +30,32 @@ app.post('/register', (req, res) => {
     return res.status(200).json({
       success: true
     })
+  })
+
+})
+
+app.post('/login', (req,res) => {
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    user.comparePassword(req.body.passord, (err, isMatch){
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+      
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id})
+      })
+    })
+
   })
 
 })
