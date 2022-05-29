46ccaa8da2ded552f095aa1ef9791df2006acbb4
Kim._.jazz <jeong011010@naver.com>
2022-05-29 14:33:24 +0900

    study React - router

--- final/client/src/App.js
+++ final/client/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route, Outlet, Link } from "react-router-dom";
 
-function App() {
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+
+export default function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <div>
+        <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
