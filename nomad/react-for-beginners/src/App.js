fb17731e2038a964bb9a19d8175b2da246bb2813
Kim._.jazz <jeong011010@naver.com>
2022-06-18 16:05:17 +0900

    nomad chap.6

--- nomad/react-for-beginners/src/App.js
+++ nomad/react-for-beginners/src/App.js
@@ -1,11 +1,24 @@
-import Button from "./Button";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [counter,setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
       <h1>props</h1>
-
-      <Button text={"continue"}/>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here"/>
+      <p>{counter}</p>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
