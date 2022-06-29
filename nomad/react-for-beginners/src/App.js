652d026a4fdb26e3b3c51d7503a03bd3422c03b1
Kim._.jazz <jeong011010@naver.com>
2022-06-29 18:25:53 +0900

    load movies api

--- nomad/react-for-beginners/src/App.js
+++ nomad/react-for-beginners/src/App.js
@@ -2,30 +2,21 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    });
+  const [movies, setMovies] = useState([]);
+  useEffect(()=> {
+    fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+      .then((response) => response.json)
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
   }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `({coins.length})`}</h1>
-      {loading ? <string>Loading...</string> : <select>
-        {coins.map((coin) => (
-          <option>
-            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
-          </option>
-        ))}
-      </select>}
-      
-      
-
-    </div>
-  );
+  return 
+  <div>
+    {loading ? <h1>Loading...</h1> : null};
+  </div>;
 }
 
 export default App;
