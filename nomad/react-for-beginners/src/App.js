f81283e8106b6888794df8835e0179aef63b0c43
Kim._.jazz <jeong011010@naver.com>
2022-06-29 18:31:15 +0900

    update load api with await

--- nomad/react-for-beginners/src/App.js
+++ nomad/react-for-beginners/src/App.js
@@ -3,18 +3,20 @@ import {useEffect, useState} from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(()=> {
-    fetch(
+  const getMoveis = async() => {
+    const json = await
+      (await fetch(
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-      .then((response) => response.json)
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(()=> {
+    getMoveis();
   }, []);
-  return 
-  <div>
+  console.log(movies);
+  return <div>
     {loading ? <h1>Loading...</h1> : null};
   </div>;
 }
